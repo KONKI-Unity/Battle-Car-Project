d4a7788a161fbb4ba6ee9f6eaf5cc7b90b6520e8
rayan <rayan.sekkat@epita.fr>
2018-03-14 18:43:00 +0100

    3eme personne camera

--- Battle Car/Assets/Modern Weapons/AK-47/gun.cs
+++ Battle Car/Assets/Modern Weapons/AK-47/gun.cs
@@ -6,14 +6,23 @@ public class gun : MonoBehaviour {
 
 	public GameObject bullet;
 	public int power;
+	public float damage;
 
 	
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetMouseButtonUp (0)) { // 0= clique gauche
 			GameObject balle = (GameObject)Instantiate(bullet, this.transform.position, Quaternion.identity);
-			balle.GetComponent<Rigidbody>().AddForce (Vector3.forward * power);
+			balle.GetComponent<Rigidbody>().AddForce (Vector3.forward * power * Time.deltaTime);
 		}
 
+	}
+
+	void OnTriggerEnter (Collider other){
+
+		var destruction = other.transform.GetComponent<Destruction> ();
+		if (destruction == null)
+			return;
+		destruction.TakeDamage (damage);
 	}
 }
