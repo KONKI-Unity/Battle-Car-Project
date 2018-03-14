98d971489f76fc4290665d489442008ec9088ce4
Pyrd <Mathieu.cailly@gmail.com>
2018-03-14 19:20:19 +0100

    Revert "-Cleaning"
    This reverts commit 209de384277929c709bb4c9ca0ace9cec43afc4d.

---
+++ Battle Car/Assets/Modern Weapons/AK-47/gun.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour {
+
+	public GameObject bullet;
+	public int power;
+	public float damage;
+
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonUp (0)) { // 0= clique gauche
+			GameObject balle = (GameObject)Instantiate(bullet, this.transform.position, Quaternion.identity);
+			balle.GetComponent<Rigidbody>().AddForce (Vector3.forward * power * Time.deltaTime);
+		}
+
+	}
+
+	void OnTriggerEnter (Collider other){
+
+		var destruction = other.transform.GetComponent<Destruction> ();
+		if (destruction == null)
+			return;
+		destruction.TakeDamage (damage);
+	}
+}
