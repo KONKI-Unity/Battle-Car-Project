09b0a1b8bdd377d22bd1b5d458841a7675670afd
rayan <rayan.sekkat@epita.fr>
2018-03-14 15:43:43 +0100

    gun

---
+++ Battle Car/Assets/bullet/Script/gun.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gun : MonoBehaviour {
+
+	public GameObject bullet;
+	public int power;
+
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonUp (0)) { // 0= clique gauche
+			GameObject balle = (GameObject)Instantiate(bullet, this.transform.position, Quaternion.identity);
+			balle.GetComponent<Rigidbody>().AddForce (Vector3.forward * power);
+		}
+
+	}
+}
