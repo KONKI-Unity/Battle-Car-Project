8564fceeb91dfbfa5540610f7f07ae7743c42969
rayan <rayan.sekkat@epita.fr>
2018-03-15 02:47:46 +0100

    Menu pause

---
+++ Battle Car/Assets/UI/PauseMenu.cs
@@ -0,0 +1,47 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+	public static bool GameIsPaused = false;
+
+	public GameObject PauseMenuUI;
+
+	void Update(){
+	
+		if (Input.GetKeyDown (KeyCode.Escape)) {
+		
+			if (GameIsPaused) {
+			
+				Resume ();
+			} else {
+			
+				Pause ();
+			}
+		}
+	}
+
+	public void Resume(){
+		PauseMenuUI.SetActive (false);
+		Time.timeScale = 1f;
+		GameIsPaused = false;
+	}
+
+	void Pause(){
+		PauseMenuUI.SetActive (true);
+		Time.timeScale = 0f;
+		GameIsPaused = true;
+	}
+
+	public void LoadMenu()
+	{
+		SceneManager.LoadScene ("Menu");
+	}
+
+	public void QuitGame(){
+		Debug.Log ("Quitting game...");
+		Application.Quit ();
+	}
+}
