6983d7822a7c19a5d86adc56ec9801250ff685e2
rayan <rayan.sekkat@epita.fr>
2018-04-25 17:46:30 +0200

    Health bar
    barre de vie
    (plus qu'a mêtre les degats et a modifié l'image du canvas "vie")

---
+++ Battle Car/Assets/PlayerStats.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StatsPlayer : MonoBehaviour {
+
+
+	public Image vie;
+	public float life, maxlife; 
+	public Text pdv;
+
+	// Use this for initialization
+	void Start () {
+		life = maxlife;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		vie.fillAmount = life / maxlife;
+		pdv.text = life.ToString () + "/" + maxlife.ToString ();
+	}
+}
