5fd6b52acc8f635278a631e69a6ea12f745007f1
Pyrd <Mathieu.cailly@gmail.com>
2018-05-02 09:33:23 +0200

    -fixing player model + adding turretmodel

---
+++ Battle Car/Assets/Script/TurretScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretScript : MonoBehaviour {
+
+    [SerializeField]
+    Transform aimTarget;
+
+
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
