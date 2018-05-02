2716728a729a73b0e64012815ecf4b3af6ad6f6e
Pyrd <Mathieu.cailly@gmail.com>
2018-05-02 08:40:52 +0200

    -adding assets

---
+++ Battle Car/Assets/Imported/CarModel/Scripts/GUIDEdrive.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+// GUIDEdrive is the lite version of the GUIDE script
+// used to control the cars in the ARK project.
+// ARK = Arcade Racing Kit by New Creations!
+
+[RequireComponent(typeof(GOdrive))]
+
+public class GUIDEdrive : MonoBehaviour {
+
+		private GOdrive vechileGUIDE;
+
+		
+		void Start () 
+		{
+			vechileGUIDE = GetComponent<GOdrive>();
+		}
+		
+		void Update () 
+		{
+		vechileGUIDE.Thrust = Input.GetAxis("Vertical");
+		vechileGUIDE.Steering = Input.GetAxis("Horizontal");
+        if (Input.GetAxis("Vertical") > 0 || Input.GetAxis("Vertical") < 0)
+            Console.Write("gooo");
+		}
+	}
+	
